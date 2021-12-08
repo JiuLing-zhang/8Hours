857272db552df5784d26b9e05259574c47b1c38d
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-12-08 17:51:28 +0800

    Add time record service

---
+++ src/Services/TimeRecordService.cs
@@ -0,0 +1,22 @@
+﻿using _8Hours.Db;
+using _8Hours.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8Hours.Services
+{
+    internal class TimeRecordService
+    {
+        public void Add(TimeRecord item)
+        {
+            using (var db = new MyDbContext())
+            {
+                db.TimeRecords.Add(item);
+                db.SaveChanges();
+            }
+        }
+    }
+}

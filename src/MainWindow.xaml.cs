d741243af8a6d097ac89d4b27f0947ca33cddc1b
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-12-08 14:37:45 +0800

    add main window layout

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -23,6 +23,53 @@ namespace _8Hours
         public MainWindow()
         {
             InitializeComponent();
+
+            SetWindowLocation();
+        }
+        private void SetWindowLocation()
+        {
+            var workArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = workArea.Right - this.Width;
+            this.Top = (workArea.Bottom - this.Height) / 2;
+        }
+
+        private Point startPoint;
+        private void Btn_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(sender as Button);
+        }
+
+        private void Btn_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            var btn = sender as Button;
+            var currentPoint = e.GetPosition(btn);
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                btn.IsMouseCaptured &&
+                (Math.Abs(currentPoint.X - startPoint.X) >
+                    SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(currentPoint.Y - startPoint.Y) >
+                    SystemParameters.MinimumVerticalDragDistance))
+            {
+                // Prevent Click from firing
+                btn.ReleaseMouseCapture();
+                DragMove();
+            }
+        }
+        private void BtnWork_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("BtnWork_Click");
+        }
+        private void BtnStudy_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("study");
+        }
+        private void BtnIdle_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("BtnIdle_Click");
+        }
+        private void BtnMore_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("BtnMore_Click");
         }
     }
 }

96751ef01b3b1ae655810ea1d7ee00eb01aa27ca
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-12-14 12:00:10 +0800

    1.Add report window style. 2.Add part of view model with report window

---
+++ src/ViewModels/ReportViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _8Hours.ViewModels
+{
+    internal class ReportViewModel : ViewModelBase
+    {
+        public Action Close { get; set; }
+        public ICommand BtnCloseCommand { get; set; }
+        public ICommand RadioConditionIsCheckedCommand { get; set; }
+        internal ReportViewModel()
+        {
+            BtnCloseCommand = new RelayCommand(parameter => CloseClick());
+            RadioConditionIsCheckedCommand = new RelayCommand(parameter => ConditionIsChecked());
+        }
+
+        private void CloseClick()
+        {
+            Close();
+        }
+
+        private async void ConditionIsChecked()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
